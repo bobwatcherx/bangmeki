5d531a8bbd6200a62fb7e8f9e7b1bb4b779be603
bobwatcherx <52072811+bobwatcherx@users.noreply.github.com>
2023-12-29 15:43:02 +0700

    Update Player.svelte

--- src/lib/pages/Player.svelte
+++ src/lib/pages/Player.svelte
@@ -6,7 +6,7 @@
   let loading = true;
   let relatedVideos = [];
 
-  let max_page = 863
+  let max_page = 899
   
   async function related() {
   const jepang_url = "https://pooptest.vercel.app/film?kode_film=9DVR9eBBSMH";
